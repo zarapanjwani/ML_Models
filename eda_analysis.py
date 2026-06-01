3d8dfad69afd4ef2c83e3bfaa12747aba1b6190d
zarapanjwani <panjwaninargis4@gmail.com>
2026-06-01 22:35:56 +0530

    Update eda_analysis.py

--- eda_analysis.py
+++ eda_analysis.py
@@ -1,32 +1,16 @@
 import pandas as pd
-
-# Load dataset
 file = pd.read_csv("WindPower.csv")
-
-# Show first 5 rows
 print("First 5 Rows")
 print(file.head())
-
-# Dataset shape
 print("\nDataset Shape")
 print(file.shape)
-
-# Column names
 print("\nColumn Names")
 print(file.columns)
-
-# Information about dataset
 print("\nDataset Information")
 print(file.info())
-
-# Check null values
 print("\nMissing Values")
 print(file.isnull().sum())
-
-# Statistical summary
 print("\nStatistical Summary")
 print(file.describe())
-
-# Check duplicate rows
 print("\nDuplicate Rows")
 print(file.duplicated().sum())
